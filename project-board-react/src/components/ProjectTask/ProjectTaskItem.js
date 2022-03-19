f1bcc4d223acd9002e237eeda0c87fae1f755b4b
Shubham Goel <shubhamgoel2525@gmail.com>
2022-03-19 22:52:41 +0530

    refactor: refactor ProjectTaskItem to functional component

--- project-board-react/src/components/ProjectTask/ProjectTaskItem.js
+++ project-board-react/src/components/ProjectTask/ProjectTaskItem.js
@@ -5,35 +5,39 @@ import { connect } from "react-redux";
 
 import { deleteProjectTask } from "../../actions/projectTaskActions";
 
-class ProjectTaskItem extends React.Component {
-  onDeleteClick(projectTaskId) {
-    this.props.deleteProjectTask(projectTaskId);
-  }
+const ProjectTaskItem = ({ projectTask, deleteProjectTask }) => {
+  const onDeleteClick = (projectTaskId) => {
+    deleteProjectTask(projectTaskId);
+  };
 
-  render() {
-    const { projectTask } = this.props;
+  return (
+    <div className="card mb-1 bg-light">
+      <div className="card-header text-primary">ID: {projectTask.id}</div>
 
-    return (
-      <div className="card mb-1 bg-light">
-        <div className="card-header text-primary">ID: {projectTask.id}</div>
+      <div className="card-body bg-light">
+        <h5 className="card-title">{projectTask.summary}</h5>
 
-        <div className="card-body bg-light">
-          <h5 className="card-title">{projectTask.summary}</h5>
+        <p className="card-text text-truncate ">
+          {projectTask.acceptanceCriteria}
+        </p>
 
-          <p className="card-text text-truncate ">
-            {projectTask.acceptanceCriteria}
-          </p>
+        <Link
+          to={`/update-project-task/${projectTask.id}`}
+          className="btn btn-primary"
+        >
+          View / Update
+        </Link>
 
-          <Link to={`/update-project-task/${projectTask.id}`} className="btn btn-primary">
-            View / Update
-          </Link>
-
-          <button className="btn btn-danger ml-4" onClick={this.onDeleteClick.bind(this, projectTask.id)}>Delete</button>
-        </div>
+        <button
+          className="btn btn-danger ml-4"
+          onClick={() => onDeleteClick(projectTask.id)}
+        >
+          Delete
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 ProjectTaskItem.propTypes = {
   deleteProjectTask: PropTypes.func.isRequired,
