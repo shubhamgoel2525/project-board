0cb01302a98c879d583de00006120fb1a26b9116
Shubham Goel <shubhamgoel2525@gmail.com>
2022-04-01 00:20:08 +0530

    feat: add ability to register user

--- project-board-spring/src/main/java/com/shubham/projectboardspring/service/JwtUserDetailsService.java
+++ project-board-spring/src/main/java/com/shubham/projectboardspring/service/JwtUserDetailsService.java
@@ -2,23 +2,45 @@ package com.shubham.projectboardspring.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.shubham.projectboardspring.dao.UserDao;
+import com.shubham.projectboardspring.domain.DAOUser;
+import com.shubham.projectboardspring.domain.UserDTO;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		DAOUser user = userDao.findByUsername(username);
 		
-		if ("shubham".equals(username)) {
-			return new User("shubham", "$2a$10$pPjGb9sowQ52Yc3Gf4GhlObas4cBbju6jc88QORgQpFC72Qz4fvpu", new ArrayList<>());
-		} else {
+		if (user == null) {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
+		
+		return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+	}
+	
+	public DAOUser save(UserDTO user) {
+		
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		return userDao.save(newUser);
 	}
 
 }
