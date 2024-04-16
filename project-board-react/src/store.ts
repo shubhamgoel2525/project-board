25195b9f40e87a7fa47cb6963e61252df26d5c0f
Shubham Goel <shubhamgoel2525@gmail.com>
2024-04-17 00:34:01 +0530

    feat: upgrade react deps

--- project-board-react/src/store.ts
+++ project-board-react/src/store.ts
@@ -1,28 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION__: any;
-  }
-}
 
 const initialState = {};
 const middleware = [thunk];
-const REDUX_DEV_TOOLS =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const isChromeOrFirefox =
-  window.navigator.userAgent.includes("Chrome") ||
-  window.navigator.userAgent.includes("Firefox");
 
 const store = createStore(
   rootReducer,
   initialState,
-  isChromeOrFirefox
-    ? compose(applyMiddleware(...middleware), REDUX_DEV_TOOLS)
-    : compose(applyMiddleware(...middleware))
+  compose(applyMiddleware(...middleware))
 );
 
 export type RootState = ReturnType<typeof store.getState>;
